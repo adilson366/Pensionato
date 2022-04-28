c355338b54bdd74ebff3d2050255a9cc66f025e1
Adilson Junior <adilsoncamposjunior@hotmail.com>
2022-04-27 22:29:27 -0300

    1

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Pencionato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Quarto[] vect = new Quarto[10];
+
+            Console.WriteLine("Digite o numero de estudantes para entrada: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Digite o nome do estudante: ");
+                string nome = Console.ReadLine();
+                Console.Write("Digite o email do estudante: ");
+                string email = Console.ReadLine();
+                Console.Write("Digite o numero do quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+
+                vect[quarto] = new Quarto(nome, email, quarto);
+            }
+
+            foreach (Quarto item in vect)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
---
+++ Quarto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pencionato
+{
+    class Quarto
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int NumeroQuarto { get; set; }
+
+        public Quarto(string nome, string email, int numeroQuarto)
+        {
+            Nome = nome;
+            Email = email;
+            NumeroQuarto = numeroQuarto;
+        }
+
+
+        public override string ToString()
+        {
+            return "Nome: "
+                + Nome
+                + "  Email: "
+                + Email
+                + "  Quarto: "
+                + NumeroQuarto;
+        }
+
+    }
+}
